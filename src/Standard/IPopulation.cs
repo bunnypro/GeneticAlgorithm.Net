0074a8ad649452d3e2b750f3cecab0f620bfa406
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-03 12:28:22 +0700

    IPopulation should accept raw offspring by default and should manage for chromosomes disticntion

--- src/Standard/IPopulation.cs
+++ src/Standard/IPopulation.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
 
 namespace Bunnypro.GeneticAlgorithm.Standard
 {
     public interface IPopulation
     {
+        ImmutableHashSet<IChromosome> Chromosomes { get; }
+        
         void Initialize();
-        void StoreOffspring(int generationNumber, ImmutableHashSet<IChromosome> offspring);
+        void StoreOffspring(int generationNumber, IEnumerable<IChromosome> offspring);
         void Reset();
     }
 }
