efbaf2503dd15fa68047cf9c35b6a6436ab172a4
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-04 23:40:11 +0700

    add a doc block

--- src/Standard/IEvolutionStrategy.cs
+++ src/Standard/IEvolutionStrategy.cs
@@ -4,7 +4,14 @@ namespace Bunnypro.GeneticAlgorithm.Standard
 {
     public interface IEvolutionStrategy
     {
+        /// <summary>
+        /// This method responsible for preparing population before being evolved
+        /// eg. Population Initialization,
+        ///     Store Population object to be used in Execute method
+        /// </summary>
+        /// <param name="population"></param>
         void Prepare(IPopulation population);
+        
         IEnumerable<IChromosome> Execute();
     }
 }
