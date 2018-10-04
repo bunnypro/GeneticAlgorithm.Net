e861d814300a17e33ba38be2d06393459b6d1dc3
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-04 23:36:45 +0700

    add EvolutionStrategy prepare responsibility

--- src/Core/EvolutionStrategies/EvolutionStrategy.cs
+++ src/Core/EvolutionStrategies/EvolutionStrategy.cs
@@ -5,9 +5,12 @@ namespace Bunnypro.GeneticAlgorithm.Core.EvolutionStrategies
 {
     public abstract class EvolutionStrategy : IEvolutionStrategy
     {
+        protected IPopulation Population { get; private set; }
+        
         public virtual void Prepare(IPopulation population)
         {
-            population.Initialize();
+            Population = population;
+            Population.Initialize();
         }
 
         public abstract IEnumerable<IChromosome> Execute();
