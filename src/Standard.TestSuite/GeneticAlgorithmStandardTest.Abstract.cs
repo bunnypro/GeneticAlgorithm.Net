b6ea678421e2d388d71c73f434591145bf698edd
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-07 17:50:41 +0700

    add doc block

--- src/Standard.TestSuite/GeneticAlgorithmStandardTest.Abstract.cs
+++ src/Standard.TestSuite/GeneticAlgorithmStandardTest.Abstract.cs
@@ -2,6 +2,10 @@ namespace Bunnypro.GeneticAlgorithm.Standard.TestSuite
 {
     public abstract partial class GeneticAlgorithmStandardTest
     {
+        /// <summary>
+        /// This method returns a fresh instance implementation of IGeneticAlgorithm
+        /// </summary>
+        /// <returns></returns>
         protected abstract IGeneticAlgorithm GeneticAlgorithm();
     }
 }
