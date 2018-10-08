a396887adf2f6cfea066889e209880c37efb5f00
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-08 16:08:40 +0700

    little cleanup

--- src/Standard/IEvolutionStrategy.cs
+++ src/Standard/IEvolutionStrategy.cs
@@ -1,12 +1,10 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
 
 namespace Bunnypro.GeneticAlgorithm.Standard
 {
     public interface IEvolutionStrategy
     {
         void Prepare(IEnumerable<IChromosome> initialChromosomes);
-
         IEnumerable<IChromosome> GenerateOffspring(IEnumerable<IChromosome> parents);
     }
 }
