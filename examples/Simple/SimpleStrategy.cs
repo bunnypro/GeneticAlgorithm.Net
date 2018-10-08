9c681213d26315fe135d553143eae5f8e9e1b1cf
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-08 17:30:24 +0700

    implement unimplemented method

--- examples/Simple/SimpleStrategy.cs
+++ examples/Simple/SimpleStrategy.cs
@@ -6,8 +6,11 @@ namespace Bunnypro.GeneticAlgorithm.Examples.Simple
 {
     public class SimpleStrategy : IEvolutionStrategy
     {
+        public HashSet<IChromosome> InitialChromosomes { get; private set; }
+        
         public void Prepare(IEnumerable<IChromosome> initialChromosomes)
         {
+            InitialChromosomes = new HashSet<IChromosome>(initialChromosomes);
         }
 
         public IEnumerable<IChromosome> GenerateOffspring(IEnumerable<IChromosome> parents)
