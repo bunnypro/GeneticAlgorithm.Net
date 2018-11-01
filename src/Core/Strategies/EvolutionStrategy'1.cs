6d289d081cab72139a57b31152934478c33d7dc4
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-11-01 18:25:04 +0700

    remove unnecessary IEvolutionStrategy<T>

--- src/Core/Strategies/EvolutionStrategy'1.cs
+++ src/Core/Strategies/EvolutionStrategy'1.cs
@@ -4,19 +4,19 @@ using Bunnypro.GeneticAlgorithm.Standard;
 
 namespace Bunnypro.GeneticAlgorithm.Core.Strategies
 {
-    public abstract class EvolutionStrategy<T> : IEvolutionStrategy, IEvolutionStrategy<T> where T : IChromosome
+    public abstract class EvolutionStrategy<T> : IEvolutionStrategy where T : IChromosome
     {
-        void IEvolutionStrategy.Prepare(IEnumerable<IChromosome> initialChromosomes)
+        public void Prepare(IEnumerable<IChromosome> initialChromosomes)
         {
             Prepare(initialChromosomes.Cast<T>());
         }
 
-        IEnumerable<IChromosome> IEvolutionStrategy.GenerateOffspring(IEnumerable<IChromosome> parents)
+        public IEnumerable<IChromosome> GenerateOffspring(IEnumerable<IChromosome> parents)
         {
             return GenerateOffspring(parents.Cast<T>()).Cast<IChromosome>();
         }
 
-        public abstract void Prepare(IEnumerable<T> initialChromosomes);
-        public abstract IEnumerable<T> GenerateOffspring(IEnumerable<T> parents);
+        protected abstract void Prepare(IEnumerable<T> initialChromosomes);
+        protected abstract IEnumerable<T> GenerateOffspring(IEnumerable<T> parents);
     }
 }
