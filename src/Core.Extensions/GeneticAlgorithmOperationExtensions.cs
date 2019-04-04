787d0ccb161f3756a042d0d1198fc48aa46da2ac
Mubarrid Albar <mubarrid.albar@gmail.com>
2019-04-04 19:34:53 +0700

    move any genetic operation that doesn't depend on any interanl fields or methods to extensions

---
+++ src/Core.Extensions/GeneticAlgorithmOperationExtensions.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bunnypro.GeneticAlgorithm.Core;
+
+namespace Bunnypro.GeneticAlgorithm.Core.Extensions
+{
+    public static class GeneticAlgorithmOperationExtensions
+    {
+        public static Task<bool> TryEvolveOnce(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            GeneticOperationStates states,
+            CancellationToken token)
+        {
+            return genetic.TryEvolveUntil(population, states, s => s.EvolutionCount >= 1, token);
+        }
+
+        public static async Task<IGeneticOperationStates> EvolveOnce(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            CancellationToken token = default)
+        {
+            var states = new GeneticOperationStates();
+            await genetic.EvolveUntil(population, states, s => s.EvolutionCount >= 1, token);
+            return states;
+        }
+
+        public static async Task<bool> TryEvolve(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            GeneticOperationStates states,
+            CancellationToken token)
+        {
+            try
+            {
+                await genetic.Evolve(population, states, token);
+            }
+            catch (OperationCanceledException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static Task Evolve(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            GeneticOperationStates states,
+            CancellationToken token)
+        {
+            return genetic.EvolveUntil(population, states, _ => false, token);
+        }
+
+        public static async Task<bool> TryEvolveUntil(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            GeneticOperationStates states,
+            Func<IGeneticOperationStates, bool> termination,
+            CancellationToken token)
+        {
+            try
+            {
+                await genetic.EvolveUntil(population, states, termination, token);
+            }
+            catch (OperationCanceledException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static async Task<IGeneticOperationStates> EvolveUntil(
+            this IGeneticAlgorithm genetic,
+            IPopulation population,
+            Func<IGeneticOperationStates, bool> termination)
+        {
+            var states = new GeneticOperationStates();
+            await genetic.EvolveUntil(population, states, termination, default);
+            return states;
+        }
+    }
+}
