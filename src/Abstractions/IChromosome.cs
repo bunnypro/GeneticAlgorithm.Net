b836be2fa4ad957a1c75580fbf208105835e3d6d
albar <mubarrid.albar@outlook.com>
2019-07-03 19:54:44 +0700

    add gene to chromosome

--- src/Abstractions/IChromosome.cs
+++ src/Abstractions/IChromosome.cs
@@ -1,7 +1,10 @@
+using System.Collections.Immutable;
+
 namespace Bunnypro.GeneticAlgorithm.Abstractions
 {
     public interface IChromosome
     {
         double Fitness { get; }
+        ImmutableArray<object> Gene { get; }
     }
 }
