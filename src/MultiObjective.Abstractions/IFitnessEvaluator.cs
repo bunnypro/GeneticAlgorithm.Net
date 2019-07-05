445397bf0e177ac74231be1c7146adbcd524a909
albar <mubarrid.albar@outlook.com>
2019-07-05 23:24:05 +0700

    make moga fitness evaluator to evaluate all at once

--- src/MultiObjective.Abstractions/IFitnessEvaluator.cs
+++ src/MultiObjective.Abstractions/IFitnessEvaluator.cs
@@ -5,7 +5,6 @@ namespace Bunnypro.GeneticAlgorithm.MultiObjective.Abstractions
 {
     public interface IFitnessEvaluator<T> where T : Enum
     {
-        double Evaluate(IChromosome<T> chromosome);
         void EvaluateAll(IEnumerable<IChromosome<T>> chromosomes);
     }
 }
