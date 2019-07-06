a95a18b015461333a25cff20850d120f4c9b5c2a
albar <mubarrid.albar@outlook.com>
2019-07-06 22:07:01 +0700

    little cleanup

--- src/MultiObjective.NSGA2/NSGA2'1.cs
+++ src/MultiObjective.NSGA2/NSGA2'1.cs
@@ -17,14 +17,11 @@ namespace Bunnypro.GeneticAlgorithm.MultiObjective.NSGA2
         private readonly IFitnessEvaluator<T> _fitnessEvaluator;
         private readonly IDistinctMultiObjectiveGeneticOperation<T> _offspringSelection = new OffspringSelection<T>();
 
-        public NSGA2(IMultiObjectiveGeneticOperation<T> reproduction,
-            IObjectiveValuesEvaluator<T> objectiveEvaluator)
+        public NSGA2(IMultiObjectiveGeneticOperation<T> reproduction, IObjectiveValuesEvaluator<T> objectiveEvaluator)
+            : this(reproduction, objectiveEvaluator, new NormalizedObjectiveValuesFitnessEvaluator<T>())
         {
-            _reproduction = reproduction;
-            _objectiveEvaluator = objectiveEvaluator;
-            _fitnessEvaluator = new NormalizedObjectiveValuesFitnessEvaluator<T>();
         }
-        
+
         public NSGA2(IMultiObjectiveGeneticOperation<T> reproduction,
             IObjectiveValuesEvaluator<T> objectiveEvaluator,
             IFitnessEvaluator<T> fitnessEvaluator)
