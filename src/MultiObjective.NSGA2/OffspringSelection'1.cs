1bcc37119b2e3ad4790dfbb1f11081d8f4aeb24a
Mubarrid Albar <mubarrid.albar@outlook.com>
2019-11-12 19:59:43 +0700

    improve offspring selection

--- src/MultiObjective.NSGA2/OffspringSelection'1.cs
+++ src/MultiObjective.NSGA2/OffspringSelection'1.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Bunnypro.GeneticAlgorithm.MultiObjective.Abstractions;
+using Bunnypro.GeneticAlgorithm.MultiObjective.Primitives;
 using Bunnypro.GeneticAlgorithm.Primitives;
 
 namespace Bunnypro.GeneticAlgorithm.MultiObjective.NSGA2
@@ -12,10 +13,12 @@ namespace Bunnypro.GeneticAlgorithm.MultiObjective.NSGA2
     public class OffspringSelection<T> : IDistinctMultiObjectiveGeneticOperation<T> where T : Enum
     {
         private readonly IParetoSort<T> _sorter;
+        private readonly IReadOnlyDictionary<T, double> _coefficients;
 
         public OffspringSelection(IReadOnlyDictionary<T, double> coefficients)
         {
             _sorter = new FastNonDominatedSort<T>(coefficients);
+            _coefficients = coefficients;
         }
 
         public Task<ImmutableHashSet<IChromosome<T>>> Operate(IEnumerable<IChromosome<T>> parents,
@@ -41,21 +44,46 @@ namespace Bunnypro.GeneticAlgorithm.MultiObjective.NSGA2
 
             var remains = capacity.Minimum - elite.Count;
             var crowdingDistance = lastFront.ToDictionary(c => c, c => 0d);
+            var normalizedComparable = Normalize(lastFront.Union(elite));
 
             foreach (var objective in Enum.GetValues(typeof(T)).Cast<T>())
             {
-                var sorted = lastFront.OrderBy(c => c.ObjectiveValues[objective]).ToArray();
+                var sorted = normalizedComparable.OrderBy(c => c.Value[objective]).ToArray();
                 for (var i = 0; i < sorted.Length; i++)
                 {
-                    var lowerValue = i == 0 ? 0 : sorted[i - 1].ObjectiveValues[objective];
-                    var upperValue = i == sorted.Length - 1 ? 0 : sorted[i + 1].ObjectiveValues[objective];
+                    if (!crowdingDistance.ContainsKey(sorted[i].Key)) continue;
+                    var lowerValue = i == 0 ? sorted[i].Value[objective] : sorted[i - 1].Value[objective];
+                    var upperValue = i == sorted.Length - 1 ? sorted[i].Value[objective] : sorted[i + 1].Value[objective];
                     var distance = Math.Abs(upperValue - lowerValue);
-                    crowdingDistance[sorted[i]] += distance;
+                    crowdingDistance[sorted[i].Key] += distance;
                 }
             }
 
             elite.UnionWith(crowdingDistance.OrderByDescending(d => d.Value).Select(d => d.Key).Take(remains));
             return Task.FromResult(elite.ToImmutable());
+        }
+
+        private IEnumerable<KeyValuePair<IChromosome<T>, ObjectiveValues<T>>> Normalize(IEnumerable<IChromosome<T>> chromosomes)
+        {
+            var comparable = chromosomes.ToArray();
+            var objectives = Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+            var normalizers = objectives
+                .ToDictionary<T, T, Func<double, double>>(o => o, objective =>
+                {
+                    var sign = Math.Sign(_coefficients[objective]);
+                    var ordered = comparable.OrderByDescending(c => c.ObjectiveValues[objective] * sign).ToArray();
+                    var minimumValue = ordered.Last().ObjectiveValues[objective];
+                    var maximumValue = ordered.First().ObjectiveValues[objective];
+                    var rangeValue = Math.Abs(maximumValue - minimumValue);
+                    return value => rangeValue <= 0 ? 1 : (Math.Abs(value - minimumValue) / rangeValue);
+                });
+
+            return comparable.Select(c =>
+            {
+                var values = objectives.ToDictionary(o => o,
+                    objective => normalizers[objective].Invoke(c.ObjectiveValues[objective]));
+                return new KeyValuePair<IChromosome<T>, ObjectiveValues<T>>(c, new ObjectiveValues<T>(values));
+            });
         }
     }
 }
